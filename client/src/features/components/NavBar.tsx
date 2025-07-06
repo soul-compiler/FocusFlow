0ef4fdb675dd566a67bfc39122decb8a5c58d9ab
soul-compiler <santiagomc.dev@gmail.com>
2025-07-06 16:48:34 -0500

    Logo on navbar

--- client/src/features/components/NavBar.tsx
+++ client/src/features/components/NavBar.tsx
@@ -6,52 +6,57 @@ export default function NavBar(props: { isLogged: boolean }) {
   const navigator = useNavigate();
   return (
     <nav className={style.nav}>
-      <NavLink
-        to="/"
-        className={({ isActive }) => {
-          return isActive
-            ? `${style.navItemActive} ${style.navItem}`
-            : style.navItem;
-        }}
-      >
-        Home
-      </NavLink>
-      {!props.isLogged ? (
-        <>
-          <NavLink
-            to="/register"
-            className={({ isActive }) => {
-              return isActive
-                ? `${style.navItemActive} ${style.navItem}`
-                : style.navItem;
-            }}
-          >
-            Register
-          </NavLink>
-          <NavLink
-            to="/login"
-            className={({ isActive }) => {
-              return isActive
-                ? `${style.navItemActive} ${style.navItem}`
-                : style.navItem;
-            }}
-          >
-            Login
-          </NavLink>
-        </>
-      ) : (
-        <>
-          <a
-            className={`${style.navItem}`}
-            onClick={() => {
-              eraseCookies("jwtToken");
-              navigator("/login");
-            }}
-          >
-            Logout
-          </a>
-        </>
-      )}
+      <div>
+        <p className={style.navTitle}>FocusFlow</p>
+      </div>
+      <div className={style.navLinks}>
+        {!props.isLogged ? (
+          <>
+            <NavLink
+              to="/register"
+              className={({ isActive }) => {
+                return isActive
+                  ? `${style.navItemActive} ${style.navItem}`
+                  : style.navItem;
+              }}
+            >
+              Register
+            </NavLink>
+            <NavLink
+              to="/login"
+              className={({ isActive }) => {
+                return isActive
+                  ? `${style.navItemActive} ${style.navItem}`
+                  : style.navItem;
+              }}
+            >
+              Login
+            </NavLink>
+          </>
+        ) : (
+          <>
+            <NavLink
+              to="/"
+              className={({ isActive }) => {
+                return isActive
+                  ? `${style.navItemActive} ${style.navItem}`
+                  : style.navItem;
+              }}
+            >
+              Home
+            </NavLink>
+            <a
+              className={`${style.navItem}`}
+              onClick={() => {
+                eraseCookies("jwtToken");
+                navigator("/login");
+              }}
+            >
+              Logout
+            </a>
+          </>
+        )}
+      </div>
     </nav>
   );
 }
